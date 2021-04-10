68356301a92682ba3f6e3286dd61d31fbcb1ba8b
manOfelf <82078109+manOfelf@users.noreply.github.com>
2021-04-10 19:02:14 +0530

    Update Occurence.cpp

--- Binary Search/Occurence.cpp
+++ Binary Search/Occurence.cpp
@@ -5,72 +5,72 @@
 using namespace std;
 
 //finding last occurence
-int Last(vector<int>arr, int element){
-  
-	int l=0,r=arr.size()-1,last=-1;
-      while(l<=r)
-      {
-          int mid=(l+r)/2;
-          if(arr[mid]<element)
+int Last(vector<int>arr, int element) {
+
+    int l=0,r=arr.size()-1,last=-1;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(arr[mid]<element)
             l=mid+1;
-          else if(arr[mid]>element)
+        else if(arr[mid]>element)
             r=mid-1;
-          else
-          {
+        else
+        {
             last=mid;
             l=mid+1;
-          }
-      }
-	return last;
+        }
+    }
+    return last;
 }
 
 //finding first occurence
-int First(vector<int>arr, int element){
-  
-	    int l=0,r=arr.size()-1,first=-1;
-      while(l<=r)
-      {
-          int mid=(l+r)/2;
-          if(arr[mid]<element)
+int First(vector<int>arr, int element) {
+
+    int l=0,r=arr.size()-1,first=-1;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(arr[mid]<element)
             l=mid+1;
-          else if(arr[mid]>element)
+        else if(arr[mid]>element)
             r=mid-1;
-          else
-          {
+        else
+        {
             first=mid;
             r=mid-1;
-          }
-      }
-	return first;
+        }
+    }
+    return first;
 }
 
 int Occurence(vector<int> arr, int element)
 {
-	int l=0,r=arr.size()-1;
-  
-	int count=Last(arr,element)-First(arr,element)+1;
+    int l=0,r=arr.size()-1;
 
-	//return count of the occurence of given element.
-	if(count==1)
-	return 0;
-	else
-	return count;
+    int count=Last(arr,element)-First(arr,element)+1;
+
+    //return count of the occurence of given element.
+    if(count==1)
+        return 0;
+    else
+        return count;
 }
 
 int main() {
-  
-      int n,element;
-      vector<int> arr;    
-      cin>>n>>element;
-      for(int i=0;i<n;i++){
-        
+
+    int n,element;
+    vector<int> arr;
+    cin>>n>>element;
+    for(int i=0; i<n; i++) {
+
         int tem;
         cin>>tem;
         arr.push_back(tem);
-      }
-	    cout<<Occurence(arr,element)<<endl;
-  
-	return 0;
+    }
+    cout<<Occurence(arr,element)<<endl;
+
+    return 0;
 }
 
 
